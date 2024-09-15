08cd8c5705305654d942b325588607ba8745fce0
Max van Deurzen <m.vandeurzen@atabix.nl>
2024-09-15 16:11:08 +0200

    init ios

--- App.tsx
+++ App.tsx
@@ -76,20 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Step One">Hello let me see your images plox</Section>
         </View>
       </ScrollView>
     </SafeAreaView>
